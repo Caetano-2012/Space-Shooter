6cebede41ee03489ae89763848dfe34c0d04f896
Caetano Bordin <hyperplayer2012@gmail.com>
2026-01-30 09:47:51 -0300

    Criação/movimento das naves e tiros

--- main.js
+++ main.js
@@ -56,14 +56,17 @@ const teclaSolta = (tecla) => {
 
 const moveNave = () => {
     posicaoHorizontal += direcaoHorizontal * velocidadeNave;
-    posicaoVetical += direcaoVetical * velocidadeNave;
-
+    posicaoVertical += direcaoVertical * velocidadeNave;
     if(posicaoHorizontal < 0) {
         posicaoHorizontal = 0;
     } else if (posicaoHorizontal + larguraNave > larguraCenario) {
         posicaoHorizontal = larguraCenario - larguraNave;
     }
-
+    if (posicaoVertical < 0) {
+        posicaoVertical = 0;
+    } else if (posicaoVertical + alturaNave > alturaCenario) {
+        posicaoVertical = alturaCenario - alturaNave;
+    }
     nave.style.left = posicaoHorizontal + "px";
     nave.style.top = posicaoVertical + "px";
 }
@@ -85,7 +88,82 @@ document.addEventListener("keydown", (tecla) => {
 });
 
 document.addEventListener("keyup", (tecla) => {
-    if (tecla.ley === " ") {
+    if (tecla.key === " ") {
         estaAtirando = false;
     }
 })
+
+const criaTiros = (posicaoLeftTiro, posicaoTopTiro) => {
+    const tiro = document.createElement("div");
+    tiro.className = "tiro";
+    tiro.style.position = "absolute";
+    tiro.style.width = "10px";
+    tiro.style.height = "10px";
+    tiro.style.backgroundColor = "red";
+    tiro.style.left = posicaoLeftTiro + "px";
+    tiro.style.left = posicaoTopTiro + "px";
+    cenario.appendChild(tiro);
+    audioTiros();
+}
+
+const audioTiros = () => {
+    const audioDoTiro = document.createElement("audio");
+    audioDoTiro.className = "audioTiro";
+    audioDoTiro.setAttribute("tiro.mp3");
+    audioDoTiro.play();
+    cenario.appendChild(audioDoTiro);
+    audioDoTiro.addEventListener("ended", () => {
+        audioDoTiro.remove();
+    })
+}
+
+const moveTiros = () => {
+    const tiros = document.querySelectorAll(".tiro");
+    for (let i = 0; i < tiros.length; i++) {
+        if (tiros[i]) {
+            let posicaoTopTiro = tiros[i].offsetTop;
+            posicaoTopTiro -= velocidadeTiro;
+            tiros[i].style.top = posicaoTopTiro + "px";
+            if (posicaoTopTiro < -10) {
+                tiros[i].remove();
+            }
+        }
+    }
+}
+
+const naveInimigas = () => {
+    const inimigo = document.createElement("div");
+    inimigo.className = "inimigo";
+    inimigo.style.position = "absolute";
+    inimigo.setAttribute("data-vida", 5);
+    inimigo.style.width = "100px";
+    inimigo.style.height = "100px";
+    inimigo.style.backgroundImage = "enemy.gif";
+    inimigo.style.backgroundPosition = "center";
+    inimigo.style.bakgroundRepeat = "no-repeat";
+    inimigo.style.backgroundSize = "contain";
+    inimigo.style.left = Math.floor(Math.random() * (larguraCenario - larguraNave)) + "px";
+    inimigo.style.top = "-100px";
+    cenario.appendChild(inimigo);
+}
+
+const moveNaveInimigas = () => {
+    const naveInimigas = document.querySelectorAll(".inimigo");
+    for (let i = 0; i < naveInimigas.length; i++) {
+        if (naveInimigas[i]) {
+            let posicaoTopNaveInimiga = naveInimigas[i].offsetTop;
+            let posicaoLeftNaveInimiga = naveInimigas[i].offsetLeft;
+            posicaoTopNaveInimiga += velocidadeNaveInimigas;
+            naveInimigas[i].style.top = posicaoTopNaveInimiga = posicaoTopNaveInimiga + "px";
+            if (posicaoTopNaveInimiga > alturaCenario) {
+                vaidaAtual -= 5;
+                vida.textContent = `Vida: ${vidaAtual}`;
+                explosaoNaveInimigaDestruida(posicaoLeftNaveInimiga);
+                if (vidaAtual <= 0) {
+                    gameOver();
+                }
+                naveInimigas[i].remove();
+            }
+        }
+    }
+}
